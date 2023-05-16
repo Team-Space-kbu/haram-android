5e0d9d27aadf2b37a6cdabf0ebcc12878a1fff88
mookseong <kmkook12312@gmail.com>
2023-05-16 16:56:42 +0900

    fix : 이름 변경으로 인한 Binding 재설정

--- app/src/main/java/com/space/haram_android/ui/home/HomeFragment.kt
+++ app/src/main/java/com/space/haram_android/ui/home/HomeFragment.kt
@@ -2,10 +2,10 @@ package com.space.haram_android.ui.home
 
 import androidx.lifecycle.ViewModelProvider
 import com.space.haram_android.R
-import com.space.haram_android.databinding.FragmentMainBinding
+import com.space.haram_android.databinding.FragmentHomeBinding
 import com.space.haram_android.ui.base.BaseFragment
 
-class HomeFragment : BaseFragment<FragmentMainBinding, HomeViewModel>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
 
     companion object {
         fun newInstance() = HomeFragment()
