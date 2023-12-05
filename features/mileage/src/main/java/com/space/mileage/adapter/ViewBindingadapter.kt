ccb7e7626af83cdc2200e21580428b17b7ae2088
mookseong <kmkook12312@gmail.com>
2023-12-05 23:02:39 +0900

    fix : mileage 화면 버그 수정

--- features/mileage/src/main/java/com/space/mileage/adapter/ViewBindingadapter.kt
+++ features/mileage/src/main/java/com/space/mileage/adapter/ViewBindingadapter.kt
@@ -1,7 +1,6 @@
 package com.space.mileage.adapter
 
 import android.graphics.Color
-import android.util.Log
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
@@ -10,9 +9,9 @@ fun layoutType(
     textView: TextView,
     string: String
 ) {
-    if (string.contains("조정")) {
-        textView.setTextColor(
-            Color.parseColor("#4B81EE")
-        )
+    if (string == "조정") {
+        textView.setTextColor(Color.parseColor("#4B81EE"))
+    }else{
+        textView.setTextColor(Color.parseColor("#707070"))
     }
 }
