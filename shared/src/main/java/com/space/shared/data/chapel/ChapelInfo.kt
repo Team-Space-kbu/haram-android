184e6d8ee4f331b44d7543df551cd767d30679bc
mookseong <kmkook12312@gmail.com>
2024-04-29 11:58:01 +0900

    feat : 남은 일수 추가 및 `일`추가

--- shared/src/main/java/com/space/shared/data/chapel/ChapelInfo.kt
+++ shared/src/main/java/com/space/shared/data/chapel/ChapelInfo.kt
@@ -1,9 +1,23 @@
 package com.space.shared.data.chapel
 
+import timber.log.Timber
+
 data class ChapelInfo(
     val regulateDays: String,
     val attendanceDays: String,
     val lateDays: String,
     val confirmationDays: String,
     val entireDays: String
-)
+) {
+
+    fun countDaysRemaining(): String {
+        return try {
+            (regulateDays.toInt() - confirmationDays.toInt()).toString()
+        } catch (e: Throwable) {
+            Timber.i(e.message)
+            "0"
+        }
+    }
+
+}
+
