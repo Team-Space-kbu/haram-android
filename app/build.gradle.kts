4792aa1220d126b857e88ab7c323d93f04b6852d
mookseong <kmkook12312@gmail.com>
2024-10-14 23:51:33 +0900

    build: 버전 정보 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 
     defaultConfig {
         applicationId = "com.space.biblemon"
-        versionCode = 120
-        versionName = "1.2.0"
+        versionCode = 122
+        versionName = "1.2.2"
     }
     signingConfigs {
         create("configName") {
