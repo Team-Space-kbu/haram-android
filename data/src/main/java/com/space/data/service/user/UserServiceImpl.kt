28ca645187e9c541693c5179117748ef894f1d4f
mookseong <kmkook12312@gmail.com>
2024-10-14 23:51:19 +0900

    clean: 불필요 코드제거

--- data/src/main/java/com/space/data/service/user/UserServiceImpl.kt
+++ data/src/main/java/com/space/data/service/user/UserServiceImpl.kt
@@ -25,7 +25,7 @@ import kotlinx.coroutines.runBlocking
 import retrofit2.HttpException
 import javax.inject.Inject
 
-internal class UserServiceImpl @Inject constructor(
+internal class   UserServiceImpl @Inject constructor(
     private val userApi: UserApi
 ) : UserService {
     private val gson = Gson()
@@ -54,24 +54,24 @@ internal class UserServiceImpl @Inject constructor(
         val responseBody = e.response()?.errorBody()?.string()
         val errorObject = gson.fromJson(responseBody, SpaceBody::class.java)
         when (errorObject.code) {
-            "USER08" -> throw com.space.shared.exception.user.PasswordDifferent("The password you entered is different")
-            "USER09" -> throw com.space.shared.exception.user.PasswordBefore("It is the same as your existing password.")
-            "MAIL01" -> throw com.space.shared.exception.signup.IncorrectCodeException("Incorrect code!!")
-            "MAIL02" -> throw com.space.shared.exception.signup.ExpirationCodeException("Code has expired.")
-            "MAIL04" -> throw com.space.shared.exception.signup.FormatIncorrectException("The format does not match. @bible.ac.kr")
-            "MAIL05" -> throw com.space.shared.exception.signup.ToMuchRequestException("Too many email requests")
-            "USER14" -> throw com.space.shared.exception.signup.UserIdFormatException("User ID format is incorrect. It should be alphanumeric and between 4 to 30 characters.")
-            "USER15" -> throw com.space.shared.exception.signup.EmailFormatException("Email format is incorrect. It should be in the format of @bible.ac.kr and between 2 to 255 characters.")
-            "USER16" -> throw com.space.shared.exception.signup.PasswordFormatException("Password format is incorrect. It should be between 8 to 255 characters.")
-            "USER17" -> throw com.space.shared.exception.signup.NicknameFormatException("Nickname format is incorrect. It should be alphanumeric and between 2 to 15 characters.")
-            "USER18" -> throw com.space.shared.exception.signup.EmailCodeFormatException("Email code should be exactly 6 characters long.")
-            "USER04" -> throw com.space.shared.exception.signup.UserAlreadyExistsException("User already exists.")
-            "USER13" -> throw com.space.shared.exception.signup.EmailInUseException("Email is already in use.")
-            "USER07" -> throw com.space.shared.exception.signup.NicknameInUseException("Nickname is already in use.")
-            "USER01", "USER02" -> throw com.space.shared.exception.NotFoundUserException("User information could not be found from the server.")
-            "USER19" -> throw com.space.shared.exception.signup.PasswordIncorrectCode("The password-only authentication code must be [0] digits long.")
-            "USER10" -> throw com.space.shared.exception.signup.ExpirationCodeException("Code has expired.")
-            "USER11", "USER06" -> throw com.space.shared.exception.signup.PasswordIncorrectCode("The authentication code is incorrect.")
+            "USER08" -> throw PasswordDifferent("The password you entered is different")
+            "USER09" -> throw PasswordBefore("It is the same as your existing password.")
+            "MAIL01" -> throw IncorrectCodeException("Incorrect code!!")
+            "MAIL02" -> throw ExpirationCodeException("Code has expired.")
+            "MAIL04" -> throw FormatIncorrectException("The format does not match. @bible.ac.kr")
+            "MAIL05" -> throw ToMuchRequestException("Too many email requests")
+            "USER14" -> throw UserIdFormatException("User ID format is incorrect. It should be alphanumeric and between 4 to 30 characters.")
+            "USER15" -> throw EmailFormatException("Email format is incorrect. It should be in the format of @bible.ac.kr and between 2 to 255 characters.")
+            "USER16" -> throw PasswordFormatException("Password format is incorrect. It should be between 8 to 255 characters.")
+            "USER17" -> throw NicknameFormatException("Nickname format is incorrect. It should be alphanumeric and between 2 to 15 characters.")
+            "USER18" -> throw EmailCodeFormatException("Email code should be exactly 6 characters long.")
+            "USER04" -> throw UserAlreadyExistsException("User already exists.")
+            "USER13" -> throw EmailInUseException("Email is already in use.")
+            "USER07" -> throw NicknameInUseException("Nickname is already in use.")
+            "USER01", "USER02" -> throw NotFoundUserException("User information could not be found from the server.")
+            "USER19" -> throw PasswordIncorrectCode("The password-only authentication code must be [0] digits long.")
+            "USER10" -> throw ExpirationCodeException("Code has expired.")
+            "USER11", "USER06" -> throw PasswordIncorrectCode("The authentication code is incorrect.")
             else -> {
                 throw e
             }
